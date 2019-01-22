945be11afb46b7e18e6fcda079e463f5ac87385f
Vuka951 <vultashin76@gmail.com>
2019-01-22 10:30:37 +0100

    webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+  target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  entry: {
+    app: ['./src/index.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'server.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ]},
+  plugins: [
+    new CleanWebpackPlugin('build', {} ),
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        // vendors chunk
+        vendor: {
+          chunks: 'all',
+          test: /node_modules/,
+        },
+      },
+    },
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        extractComments: 'all',
+        test: /\.js(\?.*)?$/i,
+        exclude: /\/node_modules/,
+      }),
+    ],
+  },
+};
